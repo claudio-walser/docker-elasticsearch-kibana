b63a4d4336f03b3ba91cdf5c4c151bf2dd5e5c6e
Claudio Walser <claudio.walser@srf.ch>
2017-03-02 18:04:03 +0100

    File itself

---
+++ start.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+echo "server.basePath: \"${KIBANA_BASE_PATH}\"" >> dkibana/config/kibana.yml;
+elasticsearch/bin/elasticsearch -E http.host=0.0.0.0 --quiet & kibana/bin/kibana --host 0.0.0.0 -Q
